3f348a47687aebcff1a6612de46c6b15a748be2b
zaporter <zack@zackporter.com>
2022-03-18 00:42:40 -0400

    Add replay flags

--- src/ReplayCommand.cc
+++ src/ReplayCommand.cc
@@ -10,11 +10,9 @@
 #include <limits>
 
 #include "Command.h"
-#include "Flags.h"
 #include "GdbServer.h"
 #include "ReplaySession.h"
 #include "ScopedFd.h"
-#include "core.h"
 #include "kernel_metadata.h"
 #include "log.h"
 #include "main.h"
@@ -72,81 +70,6 @@ ReplayCommand ReplayCommand::singleton(
     "                             be slower, but be able to tolerate missing files\n"
     "  --tty <file>               Redirect tracee replay output to <file>\n");
 
-struct ReplayFlags {
-  // Start a debug server for the task scheduled at the first
-  // event at which reached this event AND target_process has
-  // been "created".
-  FrameTime goto_event;
-
-  FrameTime singlestep_to_event;
-
-  pid_t target_process;
-
-  string target_command;
-
-  // We let users specify which process should be "created" before
-  // starting a debug session for it.  Problem is, "process" in this
-  // context is ambiguous.  It could mean the "thread group", which is
-  // created at fork().  Or it could mean the "address space", which is
-  // created at exec() (after the fork).
-  //
-  // We force choosers to specify which they mean.
-  enum { CREATED_NONE, CREATED_EXEC, CREATED_FORK } process_created_how;
-
-  // Only open a debug socket, don't launch the debugger too.
-  bool dont_launch_debugger;
-
-  // IP port to listen on for debug connections.
-  int dbg_port;
-
-  // IP host to listen on for debug connections.
-  string dbg_host;
-
-  // Whether to keep listening with a new server after the existing server
-  // detaches
-  bool keep_listening;
-
-  // Pass these options to gdb
-  vector<string> gdb_options;
-
-  // Specify a custom gdb binary with -d
-  string gdb_binary_file_path;
-
-  /* When true, echo tracee stdout/stderr writes to console. */
-  bool redirect;
-
-  /* When true, do not bind to the CPU stored in the trace file. */
-  bool cpu_unbound;
-
-  // When true make all private mappings shared with the tracee by default
-  // to test the corresponding code.
-  bool share_private_mappings;
-
-  // When nonzero, display statistics every N steps.
-  uint32_t dump_interval;
-
-  // When set, serve files from the tracer rather than asking GDB
-  // to get them from the filesystem
-  bool serve_files;
-
-  string tty;
-
-  ReplayFlags()
-      : goto_event(0),
-        singlestep_to_event(0),
-        target_process(0),
-        process_created_how(CREATED_NONE),
-        dont_launch_debugger(false),
-        dbg_port(-1),
-        dbg_host(localhost_addr),
-        keep_listening(false),
-        gdb_binary_file_path("gdb"),
-        redirect(true),
-        cpu_unbound(false),
-        share_private_mappings(false),
-        dump_interval(0),
-        serve_files(false) {}
-};
 
 static bool parse_replay_arg(vector<string>& args, ReplayFlags& flags) {
   if (parse_global_option(args)) {
@@ -570,7 +493,10 @@ int ReplayCommand::run(vector<string>& args) {
     print_help(stderr);
     return 1;
   }
+  return start_replaying(flags, trace_dir);
+}
 
+int start_replaying(ReplayFlags flags, string trace_dir){
   if (!flags.target_command.empty()) {
     flags.target_process =
         find_pid_for_command(trace_dir, flags.target_command);
@@ -596,7 +522,8 @@ int ReplayCommand::run(vector<string>& args) {
   }
   if (flags.dump_interval > 0 && !flags.dont_launch_debugger) {
     fprintf(stderr, "--stats requires -a\n");
-    print_help(stderr);
+    // TODO ZACK:
+    //print_help(stderr);
     return 2;
   }
 
--- src/ReplayCommand.h
+++ src/ReplayCommand.h
@@ -4,6 +4,10 @@
 #define RR_REPLAY_COMMAND_H_
 
 #include "Command.h"
+#include "Flags.h"
+#include "GdbServer.h"
+#include "core.h"
+#include <unistd.h>
 
 namespace rr {
 
@@ -18,7 +22,84 @@ protected:
 
   static ReplayCommand singleton;
 };
+struct ReplayFlags {
+  // Start a debug server for the task scheduled at the first
+  // event at which reached this event AND target_process has
+  // been "created".
+  FrameTime goto_event;
 
+  FrameTime singlestep_to_event;
+
+  pid_t target_process;
+
+  std::string target_command;
+
+  // We let users specify which process should be "created" before
+  // starting a debug session for it.  Problem is, "process" in this
+  // context is ambiguous.  It could mean the "thread group", which is
+  // created at fork().  Or it could mean the "address space", which is
+  // created at exec() (after the fork).
+  //
+  // We force choosers to specify which they mean.
+  enum { CREATED_NONE, CREATED_EXEC, CREATED_FORK } process_created_how;
+
+  // Only open a debug socket, don't launch the debugger too.
+  bool dont_launch_debugger;
+
+  // IP port to listen on for debug connections.
+  int dbg_port;
+
+  // IP host to listen on for debug connections.
+  std::string dbg_host;
+
+  // Whether to keep listening with a new server after the existing server
+  // detaches
+  bool keep_listening;
+
+  // Pass these options to gdb
+  std::vector<std::string> gdb_options;
+
+  // Specify a custom gdb binary with -d
+  std::string gdb_binary_file_path;
+
+  /* When true, echo tracee stdout/stderr writes to console. */
+  bool redirect;
+
+  /* When true, do not bind to the CPU stored in the trace file. */
+  bool cpu_unbound;
+
+  // When true make all private mappings shared with the tracee by default
+  // to test the corresponding code.
+  bool share_private_mappings;
+
+  // When nonzero, display statistics every N steps.
+  uint32_t dump_interval;
+
+  // When set, serve files from the tracer rather than asking GDB
+  // to get them from the filesystem
+  bool serve_files;
+
+  std::string tty;
+
+  ReplayFlags()
+      : goto_event(0),
+        singlestep_to_event(0),
+        target_process(0),
+        process_created_how(CREATED_NONE),
+        dont_launch_debugger(false),
+        dbg_port(-1),
+        dbg_host(localhost_addr),
+        keep_listening(false),
+        gdb_binary_file_path("gdb"),
+        redirect(true),
+        cpu_unbound(false),
+        share_private_mappings(false),
+        dump_interval(0),
+        serve_files(false) {}
+};
+
+
+int start_replaying(ReplayFlags flags, std::string trace_dir);
 } // namespace rr
 
 #endif // RR_REPLAY_COMMAND_H_
